470c5b820ce39d1f17e9af97830dd55ed6406e02
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-06-27 11:54:05 -0700

    solved second exercise which was doing my own power function using recursion

--- learningRecursion.js
+++ learningRecursion.js
@@ -1,5 +1,9 @@
 console.log("hi");
 
+console.log(
+  `Exercises from https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion`
+);
+
 /*
 Question 1: Sum all numbers
 Write a function called sumRange. It will take a number and return the sum of all numbers from 1 up to the number passed in.
@@ -33,3 +37,29 @@ function sumRange(n) {
 }
 
 // The function above works~
+
+/*
+
+///Question 2: Power function////
+Write a function called power which takes in a base and an exponent. If the exponent is 0, return 1.
+
+Sample:
+
+console.log(power(2, 4)); // 16
+console.log(power(2, 3)); // 8
+console.log(power(2, 2)); // 4 
+console.log(power(2, 1)); // 2
+console.log(power(2, 0)); // 1
+
+
+*/
+
+function power(base, exponent) {
+  //base case
+  if (exponent === 0) return 1;
+  else {
+    return base * power(base, exponent - 1);
+  }
+}
+
+// exercise 2 done, works
